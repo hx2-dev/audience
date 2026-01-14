75ea924561db1f8f685aaf2af07e0c91bd4e55aa
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2026-01-13 20:03:14 -0700

    :see_no_evil: Don't lint .storybook

--- eslint.config.js
+++ eslint.config.js
@@ -12,69 +12,76 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-export default tseslint.config({
-  ignores: [
-    ".next",
-    "postcss.config.js",
-    "prettier.config.js",
-    "next-env.d.ts",
-    "src/core/adapters/db/database.types.ts",
-  ],
-}, ...compat.extends("next/core-web-vitals"), {
-  files: ["**/*.ts", "**/*.tsx"],
-  plugins: {
-    drizzle,
+export default tseslint.config(
+  {
+    ignores: [
+      ".next",
+      "postcss.config.js",
+      "prettier.config.js",
+      "next-env.d.ts",
+      "src/core/adapters/db/database.types.ts",
+      ".storybook",
+    ],
   },
-  extends: [
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-    comments.recommended,
-  ],
-  rules: {
-    "@eslint-community/eslint-comments/require-description": "error",
-    "@eslint-community/eslint-comments/no-unused-disable": "error",
-    "@eslint-community/eslint-comments/no-unlimited-disable": "error",
-    "@eslint-community/eslint-comments/no-duplicate-disable": "error",
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/ban-ts-comment": [
-      "error",
-      {
-        "ts-expect-error": {
-          descriptionFormat: "^: TS\\d+ .+$",
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      drizzle,
+    },
+    extends: [
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      comments.recommended,
+    ],
+    rules: {
+      "@eslint-community/eslint-comments/require-description": "error",
+      "@eslint-community/eslint-comments/no-unused-disable": "error",
+      "@eslint-community/eslint-comments/no-unlimited-disable": "error",
+      "@eslint-community/eslint-comments/no-duplicate-disable": "error",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-expect-error": {
+            descriptionFormat: "^: TS\\d+ .+$",
+          },
         },
-      },
-    ],
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports", fixStyle: "inline-type-imports" },
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_" },
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      { checksVoidReturn: { attributes: false } },
-    ],
-    "drizzle/enforce-delete-with-where": [
-      "error",
-      { drizzleObjectName: ["db", "ctx.db"] },
-    ],
-    "drizzle/enforce-update-with-where": [
-      "error",
-      { drizzleObjectName: ["db", "ctx.db"] },
-    ],
-  },
-}, {
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
-  },
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "drizzle/enforce-delete-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
+      "drizzle/enforce-update-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
     },
   },
-}, storybook.configs["flat/recommended"]);
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  storybook.configs["flat/recommended"],
+);
